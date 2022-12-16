4a18a8ad25c4867c0641cddf43c5cc53629de210
Julian Rubisch <julian@julianrubisch.at>
2022-12-16 14:07:22 +0100

    feat: Rehydrate server-side

--- app/helpers/solder/application_helper.rb
+++ app/helpers/solder/application_helper.rb
@@ -1,6 +1,6 @@
 module Solder
   module ApplicationHelper
-    def solder_with(name, &block)
+    def solder_onto(name, &block)
       fragment = Nokogiri::HTML.fragment(capture(&block).squish)
 
       first_fragment_child = fragment.first_element_child
@@ -9,17 +9,22 @@ module Solder
       first_fragment_child["data-controller"] = "#{first_fragment_child["data-controller"]} solder".strip
       first_fragment_child["data-solder-key-value"] = solder_key(name)
 
-      # TODO rehydrate
+      # rehydrate
+      ui_state = Rails.cache.read "solder/#{solder_key(name)}"
+
+      ui_state&.each do |attribute_name, value|
+        first_fragment_child[attribute_name] = value
+      end
 
       first_fragment_child.to_html.html_safe
     end
 
     def solder_key(name)
       key = cache_fragment_name(name, skip_digest: true)
-        .flatten
-        .compact
-        .map(&:cache_key)
-        .join(":")
+              .flatten
+              .compact
+              .map(&:cache_key)
+              .join(":")
 
       key += ":#{caller.find { _1 =~ /html/ }}"
 
